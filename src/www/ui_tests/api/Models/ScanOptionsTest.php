acfa6e720e4b9bc973d57fc4ef7db1abd2a0ccdc
Shaheem Azmal M MD <shaheem.azmal@siemens.com>
2022-03-08 20:23:45 +0530

    fix(scancode): update load function and fix testcases for scan code
    Signed-off-by: Shaheem Azmal M MD <shaheem.azmal@siemens.com>

--- src/www/ui_tests/api/Models/ScanOptionsTest.php
+++ src/www/ui_tests/api/Models/ScanOptionsTest.php
@@ -79,7 +79,7 @@ namespace Fossology\UI\Api\Test\Models
         ]);
       self::$functions->shouldReceive('register_plugin')
         ->with(\Hamcrest\Matchers::identicalTo(
-          new ScanOptions(null, null, null)));
+          new ScanOptions(null, null, null, null)));
     }
 
     /**
@@ -142,7 +142,9 @@ namespace Fossology\UI\Api\Test\Models
       $decider->setOjoDecider(true);
       $decider->setNomosMonk(true);
 
-      $scanOption = new ScanOptions($analysis, $reuse, $decider);
+      $scancode = new Scancode();
+
+      $scanOption = new ScanOptions($analysis, $reuse, $decider, $scancode);
 
       $this->userDao->shouldReceive('getGroupIdByName')
         ->withArgs([$groupName])->andReturn($groupId);
